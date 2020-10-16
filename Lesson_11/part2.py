32d7ada0bc02a2fb4ffb1de5500ca2d64499ee54
RyslanaBinko <binko.ryslana@gmail.com>
2020-10-16 20:36:03 +0300

    Lesson 11, new

--- Lesson_11/part2.py
+++ Lesson_11/part2.py
@@ -1,8 +1,8 @@
-email = "fhgggg38y@gmail.com"
+email = input("Enter your email: ")
 
 
 def hide_email(email):
-    new_email = email.replace("g", "*")
+    new_email = email[:2] + "***" + "@" + "**" + email[len(email)-3:]
     print(new_email)
 
 
